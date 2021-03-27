a51a6ebbcca0664ab8d0e020cbf8616a94898b65
Neto1904 <netohsa66@gmail.com>
2021-03-27 16:49:17 -0300

    Serve stati files from uploads folder

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import orphanagesController from './controllers/orphanagesController'
 import upload from './config/multer'
+import path from 'path'
 
 const routes = express.Router()
 
 routes.post('/orphanages', upload.array('images'), orphanagesController.create)
 routes.get('/orphanages', orphanagesController.getAll)
 routes.get('/orphanages/:id', orphanagesController.getOne)
+
+routes.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 export default routes
