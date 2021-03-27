3ddf38f6f82a69cc7eeb2d9764e7d1ebc20f7b4d
Neto1904 <netohsa66@gmail.com>
2021-03-27 15:43:17 -0300

    Add routes to get all ophanages and get one orphanage

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import orphanagesController from './controllers/orphanagesController'
+import upload from './config/multer'
 
 const routes = express.Router()
 
-routes.post('/orphanages', orphanagesController.create)
+routes.post('/orphanages', upload.array('images'), orphanagesController.create)
+routes.get('/orphanages', orphanagesController.getAll)
+routes.get('/orphanages/:id', orphanagesController.getOne)
 
 export default routes
