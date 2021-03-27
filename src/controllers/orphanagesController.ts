766b52af1a85dc5d696626e30adbec313981da82
Neto1904 <netohsa66@gmail.com>
2021-03-27 16:49:53 -0300

    Add yup to validade schema

--- src/controllers/orphanagesController.ts
+++ src/controllers/orphanagesController.ts
@@ -1,15 +1,32 @@
 import { Request, Response, Express } from 'express'
 import { getRepository } from 'typeorm'
 import Orphanage from '../database/entities/orphanage'
+import OrphanageView from '../views/orphanagesView'
+import * as yup from 'yup'
 
 export default {
   async create (req: Request, res: Response) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      latitude: yup.number().required(),
+      longitude: yup.number().required(),
+      about: yup.string().required().max(300),
+      instructions: yup.string().required(),
+      openingHours: yup.string().required(),
+      openOnWeekends: yup.boolean().required(),
+      images: yup.array(yup.object().shape({
+        path: yup.string().required()
+      }))
+    })
     const images = []
     const orphanageRepository = getRepository(Orphanage)
     for (const image of req.files as Express.Multer.File[]) {
       images.push({ path: image.filename })
     }
     req.body.images = images
+    await schema.validate(req.body, {
+      abortEarly: false
+    })
     const orphanage = orphanageRepository.create(req.body)
     await orphanageRepository.save(orphanage)
     return res.status(201).json(orphanage)
@@ -17,14 +34,18 @@ export default {
 
   async getAll (req: Request, res: Response) {
     const orphanageRepository = getRepository(Orphanage)
-    const orphanages = await orphanageRepository.find()
+    const orphanages = await orphanageRepository.find({
+      relations: ['images']
+    })
     return res.status(201).json(orphanages)
   },
 
   async getOne (req: Request, res: Response) {
     const { id } = req.params
     const orphanageRepository = getRepository(Orphanage)
-    const orphanages = await orphanageRepository.findOneOrFail(id)
-    return res.status(201).json(orphanages)
+    const orphanages = await orphanageRepository.findOneOrFail(id, {
+      relations: ['images']
+    })
+    return res.status(201).json(OrphanageView.render(orphanages))
   }
 }
