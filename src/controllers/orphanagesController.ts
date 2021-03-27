6cf544cfebb0885fbb3ba412b0cadd7c900fe647
Neto1904 <netohsa66@gmail.com>
2021-03-27 15:44:01 -0300

    Add methods to list and to recover only one oprhanage info

--- src/controllers/orphanagesController.ts
+++ src/controllers/orphanagesController.ts
@@ -1,13 +1,30 @@
-import { Request, Response } from 'express'
+import { Request, Response, Express } from 'express'
 import { getRepository } from 'typeorm'
 import Orphanage from '../database/entities/orphanage'
 
 export default {
   async create (req: Request, res: Response) {
+    const images = []
     const orphanageRepository = getRepository(Orphanage)
+    for (const image of req.files as Express.Multer.File[]) {
+      images.push({ path: image.filename })
+    }
+    req.body.images = images
     const orphanage = orphanageRepository.create(req.body)
-    console.log(orphanage)
     await orphanageRepository.save(orphanage)
     return res.status(201).json(orphanage)
+  },
+
+  async getAll (req: Request, res: Response) {
+    const orphanageRepository = getRepository(Orphanage)
+    const orphanages = await orphanageRepository.find()
+    return res.status(201).json(orphanages)
+  },
+
+  async getOne (req: Request, res: Response) {
+    const { id } = req.params
+    const orphanageRepository = getRepository(Orphanage)
+    const orphanages = await orphanageRepository.findOneOrFail(id)
+    return res.status(201).json(orphanages)
   }
 }
