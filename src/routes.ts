76fd98a6da2ecd8a9fcee2818980e81e10efc255
Neto1904 <netohsa66@gmail.com>
2021-03-27 14:01:38 -0300

    Separe routes from controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,8 @@
 import express from 'express'
+import orphanagesController from './controllers/orphanagesController'
 
 const routes = express.Router()
+
+routes.post('/orphanages', orphanagesController.create)
+
+export default routes
