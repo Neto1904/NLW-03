8c2fc51d20631e40e482ab555b8867594421ac1d
Neto1904 <netohsa66@gmail.com>
2021-03-27 16:49:36 -0300

    Add error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import 'express-async-errors'
 import { createConnection } from 'typeorm'
 import routes from './routes'
+import ErrorHandler from './errors/handler'
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+app.use(ErrorHandler)
 
 app.listen(8080, async () => {
   await createConnection()
