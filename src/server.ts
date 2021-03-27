255638026baa52deab2d5631895712a55a9ba540
Neto1904 <netohsa66@gmail.com>
2021-03-27 14:02:31 -0300

    Separate routes from server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import { createConnection } from 'typeorm'
 
 const app = express()
 app.use(express.json())
 
-app.listen(8080, () => {
+app.listen(8080, async () => {
+  await createConnection()
   console.log('Server running on port 8080')
 })
