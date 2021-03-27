811a1739dfa8879b7638504a6126be388239a343
Neto1904 <netohsa66@gmail.com>
2021-03-27 15:43:27 -0300

    Add routes middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { createConnection } from 'typeorm'
+import routes from './routes'
 
 const app = express()
 app.use(express.json())
+app.use(routes)
 
 app.listen(8080, async () => {
   await createConnection()
